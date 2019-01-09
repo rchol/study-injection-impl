c8beaef6dec087c89339ca05bdeede9bf06822c1
r.chernyshev <r.chernyshev@qiwi.com>
2019-01-09 16:48:57 +0300

    ReflectionMapper tests addind

--- src/main/java/api/json/impl/ReflectionMapper.java
+++ src/main/java/api/json/impl/ReflectionMapper.java
@@ -1,5 +1,153 @@
 package api.json.impl;
 
-public class ReflectionMapper {
+import api.json.JSONCreator;
+import api.json.JSONElement;
+import api.json.JSONField;
+import api.json.JSONObject;
+import com.google.common.base.Preconditions;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.text.WordUtils;
 
+public class ReflectionMapper {
+    public <T> T createObject(JSONElement je, Class<T> targetType) {
+        Preconditions.checkNotNull(je, "source json can't be null");
+        Preconditions.checkArgument(je.isJsonObject(),"It isn't JSON object");
+        JSONObject jo = je.getAsJsonObject();
+
+        T obj;
+
+        Constructor[] ctors  = targetType.getConstructors();
+        try {
+
+            obj = (T) Stream.of(ctors)
+                .filter(ctor -> ctor.isAnnotationPresent(JSONCreator.class))
+                .findFirst()
+                .map(ctor -> createObjByCnstr(jo, ctor))
+                .orElseGet(() -> createObjByDefaultCnstr(jo, targetType));
+
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("Bean has no default or with \"JSONCreate\" annotation constructor ", e);
+        }
+
+        return obj;
+    }
+
+    private <T> T createObjByCnstr(JSONObject jo, Constructor<T> ctor) {
+        try {
+            Parameter[] ctorParams =  ctor.getParameters();
+            Preconditions.checkArgument(
+                Stream.of(ctorParams).allMatch(f->f.isAnnotationPresent(JSONField.class)),
+                "All constructor parameters must have @JSONField annotation"
+            );
+            List<Object> param = new ArrayList<>(ctorParams.length);
+            Stream.of(ctorParams)
+                .map(f -> f.getAnnotation(JSONField.class))
+                .forEach(field -> {
+                    if (!jo.has(field.name()) && field.required()) {
+                        throw new IllegalArgumentException("Missing required parameter " + field.name());
+                    } else {
+                        if (jo.has(field.name())) {
+                            MyJSONPrimitive jp = (MyJSONPrimitive) jo.get(field.name()).getAsJsonPrimitive();
+                            param.add(jp.getAsObject());
+                        }
+                        else {
+                            param.add(null);
+                        }
+                    }
+                });
+            return ctor.newInstance(param.toArray());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Bean has no JSONCreator annotation or something bad happened", e);
+        }
+    }
+
+    private <T> T createObjByDefaultCnstr(JSONObject jo, Class<T> targetType) {
+        T obj;
+        try {
+            Constructor<T> ctor = targetType.getDeclaredConstructor();
+            obj = ctor.newInstance();
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("Bean has no default constructor");
+        }
+
+
+        jo
+            .entrySet()
+            .forEach(je -> {
+                String setterFieldName = setterNameOf(je.getKey());
+                try {
+                    Method method = Stream.of(targetType.getMethods())
+                        .filter(mtd -> mtd.getName().equals(setterFieldName) && mtd.getParameterCount() == 1)
+                        .findFirst()
+                        .orElseThrow(() -> new IllegalArgumentException("Can't find setter for " + je.getKey() + " or too many parameters in the method"));
+
+                    Class parameterClass = method.getParameterTypes()[0];
+
+                    if (parameterClass.isEnum()) {
+                        Object enumValue = Stream.of(parameterClass.getEnumConstants())
+                            .filter(enumName -> enumName.toString().equals(je.getValue().getAsJsonPrimitive().getAsString()))
+                            .findFirst()
+                            .orElseThrow(() -> new IllegalArgumentException("Can't find enum value of " + je.getValue().getAsJsonPrimitive().getAsString()));
+                        method.invoke(obj, enumValue);
+                    } else if (parameterClass == Map.class) {
+                        Map<Object, Object> mapValue = new HashMap<>();
+                        JSONObject mjo = je.getValue().getAsJsonObject();
+                        mjo
+                            .entrySet()
+                            .forEach(k -> {
+                                MyJSONPrimitive mjp = (MyJSONPrimitive) k.getValue().getAsJsonPrimitive();
+                                mapValue.put(k.getKey(), mjp.getAsObject());
+                            });
+                        method.invoke(obj, mapValue);
+                    } else if (parameterClass == BigDecimal.class) {
+                        method.invoke(obj, je.getValue().getAsJsonPrimitive().getAsBigDecimal());
+                    } else {
+                        MyJSONPrimitive mjp = (MyJSONPrimitive) je.getValue().getAsJsonPrimitive();
+                        method.invoke(obj, mjp.getAsObject());
+                    }
+                } catch (IllegalArgumentException e) {
+                    throw new IllegalArgumentException("Can't map field", e);
+                } catch (Exception e) {
+                    throw new RuntimeException("Some security violations, or I dunno", e);
+                }
+            });
+
+        return obj;
+    }
+
+    /**
+     * According to common sense JSON originated from JavaScript, the appropriate naming convention of keys
+     * for JSON is and should be in camelCase regardless of what programming language the JSON was formed. (c) so
+     * But nevertheless, if we encounter underscores in field_name, we try to convert it to fieldName
+     */
+    public String fieldNameOf(String jsonFieldName) {
+
+        String name = WordUtils.capitalize(jsonFieldName, new char[]{'_'}).replaceAll("_", "");
+        StringBuilder sb = new StringBuilder();
+        sb.append(Character.toLowerCase(name.charAt(0)));
+        sb.append(name.substring(1));
+        return sb.toString();
+    }
+
+    /**
+     * @see ReflectionMapper#fieldNameOf(String)
+     * but this method will also append get as prefix
+     */
+    public String getterNameOf(String jsonFieldName) {
+        return fieldNameOf("get_" + jsonFieldName);
+    }
+
+    public String setterNameOf(String jsonFieldName) {
+        return fieldNameOf("set_"+jsonFieldName);
+    }
 }
---
+++ src/test/java/json/ReflectionMapperTest.java
@@ -0,0 +1,270 @@
+package json;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import api.json.JSONCreator;
+import api.json.JSONElement;
+import api.json.JSONField;
+import api.json.impl.ImplementedJsonParser;
+import api.json.impl.ReflectionMapper;
+import java.io.StringReader;
+import java.math.BigDecimal;
+import java.util.Map;
+import org.junit.Test;
+
+
+public class ReflectionMapperTest {
+
+    private static final ReflectionMapper mapper = new ReflectionMapper();
+
+    @Test
+    public void testFieldNameOf() throws Exception {
+        String nothingToDo = "noop";
+        assertEquals("nothing to do", nothingToDo, mapper.fieldNameOf(nothingToDo));
+
+        String idempotent = "alreadyCamelCased";
+        assertEquals("shouldn't change nothing", idempotent, mapper.fieldNameOf(idempotent));
+        assertEquals("get prefixed", "getAlreadyCamelCased", mapper.getterNameOf(idempotent));
+
+        // Bonus level
+        // In reality no one(i think) but spring handles this kind of nonsense
+        String snakeCased = "i_am_snake_cased";
+        assertEquals("We can handle snakes", "iAmSnakeCased", mapper.fieldNameOf(snakeCased));
+        assertEquals("get is prefixed", "getIAmSnakeCased", mapper.getterNameOf(snakeCased));
+    }
+
+    @Test
+    public void testMapper() {
+        String simpleJson =
+                "{\n" +
+                "  \"a\": \"a\",\n" +
+                "  \"b\": 2\n" +
+                "}";
+        JSONElement je = new ImplementedJsonParser().parse(new StringReader(simpleJson));
+        SimplePOJO sp = mapper.createObject(je, SimplePOJO.class);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalArgument() {
+        String noSuchField =
+                "{\n" +
+                "  \"a\": \"a\",\n" +
+                "  \"b\": 2,\n" +
+                "  \"c\": \"c\"\n" +
+                "}";
+        JSONElement je = new ImplementedJsonParser().parse(new StringReader(noSuchField));
+        SimplePOJO sp = mapper.createObject(je, SimplePOJO.class);
+    }
+
+    static class SimplePOJO {
+        String a;
+        int b;
+
+        public String getA() {
+            return a;
+        }
+
+        public void setA(String a) {
+            this.a = a;
+        }
+
+        public int getB() {
+            return b;
+        }
+
+        public void setB(int b) {
+            this.b = b;
+        }
+
+    }
+
+    @Test //Bonus level: hard
+    public void testBigPOJOMapping() throws Exception {
+        String paymentInfo =
+                "{\n" +
+                "  \"id\": 1254817343741,\n" +
+                "  \"toAccount\" : \"R12334\",\n" +
+                "  \"fromAccount\": \"R33212\",\n" +
+                "  \"currency\": \"RUB\",\n" +
+                "  \"fromAmount\": 100.0,\n" +
+                "  \"toAmount\": 95.0,\n" +
+                "  \"date\": 1446361200000\n" +
+                "}";
+        JSONElement je = new ImplementedJsonParser().parse(new StringReader(paymentInfo));
+        PaymentInfo pi = mapper.createObject(je, PaymentInfo.class);
+        assertEquals("ids matches", 1254817343741L, pi.getId());
+        assertEquals("to account matches", "R12334", pi.getToAccount());
+        assertEquals("from account matches", "R33212", pi.getFromAccount());
+        assertEquals("currency set correctly", Currency.RUB, pi.getCurrency());
+        assertEquals("from amount matches", new BigDecimal("100.0"), pi.getFromAmount());
+        assertEquals("to amount matches", new BigDecimal("95.0"), pi.getToAmount());
+        assertEquals("date parsed correctly", 1446361200000L, pi.getDate());
+    }
+
+    @Test //Bonus level: hardcore
+    public void testBigPOJOMapping2() throws Exception {
+        String paymentInfo =
+                "{\n" +
+                "  \"id\": 1254817343741,\n" +
+                "  \"toAccount\" : \"R12334\",\n" +
+                "  \"fromAccount\": \"R33212\",\n" +
+                "  \"currency\": \"RUB\",\n" +
+                "  \"fromAmount\": 100.0,\n" +
+                "  \"toAmount\": 95.0,\n" +
+                "  \"date\": 1446361200000,\n" +
+                "  \"extras\": {\n" +
+                "    \"a\": \"b\",\n" +
+                "    \"c\": \"d\"\n" +
+                "  }\n" +
+                "}";
+        JSONElement je = new ImplementedJsonParser().parse(new StringReader(paymentInfo));
+        PaymentInfo pi = mapper.createObject(je, PaymentInfo.class);
+        assertEquals("ids matches", 1254817343741L, pi.getId());
+        assertEquals("to account matches", "R12334", pi.getToAccount());
+        assertEquals("from account matches", "R33212", pi.getFromAccount());
+        assertEquals("currency set correctly", Currency.RUB, pi.getCurrency());
+        assertEquals("from amount matches", new BigDecimal("100.0"), pi.getFromAmount());
+        assertEquals("to amount matches", new BigDecimal("95.0"), pi.getToAmount());
+        assertEquals("date parsed correctly", 1446361200000L, pi.getDate());
+        assertEquals("extra 'a' parsed", "b", pi.getExtras().get("a"));
+        assertEquals("extra 'b' parsed", "d", pi.getExtras().get("c"));
+    }
+
+    static class PaymentInfo {
+        long id;
+        String toAccount;
+        String fromAccount;
+        Currency currency;
+        BigDecimal fromAmount;
+        BigDecimal toAmount;
+        long date;
+        Map<String, String> extras;
+
+        public String getToAccount() {
+            return toAccount;
+        }
+
+        public void setToAccount(String toAccount) {
+            this.toAccount = toAccount;
+        }
+
+        public String getFromAccount() {
+            return fromAccount;
+        }
+
+        public void setFromAccount(String fromAccount) {
+            this.fromAccount = fromAccount;
+        }
+
+        public Currency getCurrency() {
+            return currency;
+        }
+
+        public void setCurrency(Currency currency) {
+            this.currency = currency;
+        }
+
+        public BigDecimal getFromAmount() {
+            return fromAmount;
+        }
+
+        public void setFromAmount(BigDecimal fromAmount) {
+            this.fromAmount = fromAmount;
+        }
+
+        public BigDecimal getToAmount() {
+            return toAmount;
+        }
+
+        public void setToAmount(BigDecimal toAmount) {
+            this.toAmount = toAmount;
+        }
+
+        public long getDate() {
+            return date;
+        }
+
+        public void setDate(long date) {
+            this.date = date;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public Map<String, String> getExtras() {
+            return extras;
+        }
+
+        public void setExtras(Map<String, String> extras) {
+            this.extras = extras;
+        }
+    }
+
+    enum Currency {
+        RUB
+    }
+
+    @Test
+    public void testMappingByCtor() {
+        String json = "{\"R01\" : \"NIC\", \"X07\": \"+1 555 CALLME\", \"PRI\": -1}";
+        JSONElement je = new ImplementedJsonParser().parse(new StringReader(json));
+        ProprietaryPOJO info = mapper.createObject(je, ProprietaryPOJO.class);
+        assertEquals("Contractor Id mapped right", "NIC", info.getContractorId());
+        assertEquals("Extra info mapped too", "+1 555 CALLME", info.getExtraInfo());
+        assertEquals("Obligatory field found", -1, info.getPriority().intValue());
+    }
+
+    @Test
+    public void testMappingByCtorMissingObligatoryField() {
+        String json = "{\"R01\" : \"NIC\", \"X07\": \"+1 555 CALLME\"}";
+        JSONElement je = new ImplementedJsonParser().parse(new StringReader(json));
+        ProprietaryPOJO info = mapper.createObject(je, ProprietaryPOJO.class);
+        assertEquals("Contractor Id mapped right", "NIC", info.getContractorId());
+        assertEquals("Extra info mapped too", "+1 555 CALLME", info.getExtraInfo());
+        assertNull("Field must be empty", info.getPriority());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMappingByCtorMissingRequiredField() {
+        String json = "{\"X07\": \"+1 555 CALLME\"}";
+        JSONElement je = new ImplementedJsonParser().parse(new StringReader(json));
+        ProprietaryPOJO info = mapper.createObject(je, ProprietaryPOJO.class);
+        fail("Should've throw excpetion by now, missing required field!");
+    }
+
+    static class ProprietaryPOJO {
+        private final String contractorId;
+        private final String extraInfo;
+        private final Integer priority;
+        @JSONCreator
+        public ProprietaryPOJO(
+                @JSONField(name = "R01") String contractorId,
+                @JSONField(name = "X07") String extraInfo,
+                @JSONField(name = "PRI", required = false)  Integer priority
+        ) {
+            this.contractorId = contractorId;
+            this.extraInfo = extraInfo;
+            this.priority = priority;
+        }
+
+        public String getContractorId() {
+            return contractorId;
+        }
+
+        public String getExtraInfo() {
+            return extraInfo;
+        }
+
+        public Integer getPriority() {
+            return priority;
+        }
+    }
+}
